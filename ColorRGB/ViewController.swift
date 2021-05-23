948b19bfcf596157e98a260d34f1b7a9d67cc3c8
ShevtsovVA <v.a.shevtsov@yandex.ru>
2021-05-23 19:19:32 +0300

    fixed functional

--- ColorRGB/ViewController.swift
+++ ColorRGB/ViewController.swift
@@ -12,51 +12,72 @@ class ViewController: UIViewController {
     @IBOutlet weak var redValue: UILabel!
     @IBOutlet weak var greenValue: UILabel!
     @IBOutlet weak var blueValue: UILabel!
+    @IBOutlet weak var alphaValue: UILabel!
     
+  
     @IBOutlet weak var checkColor: UIView!
     
     @IBOutlet weak var redSlider: UISlider!
     @IBOutlet weak var greenSlider: UISlider!
     @IBOutlet weak var blueSlider: UISlider!
-    
+    @IBOutlet weak var alphaSlider: UISlider!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
-//        redSlider.minimumValue = 0
-//        redSlider.maximumValue = 255
+        
         redSlider.tintColor = .red
-        
-//        greenSlider.minimumValue = 0
-//        greenSlider.maximumValue = 255
+
         greenSlider.tintColor = .green
-        
-//        blueSlider.minimumValue = 0
-//        blueSlider.maximumValue = 255
+
         blueSlider.tintColor = .blue
         
-        checkColor.backgroundColor = UIColor (
-        red: CGFloat(redSlider.value),
-        green: CGFloat(greenSlider.value),
-        blue: CGFloat(blueSlider.value),
-        alpha: 1.0
-        )
+//        alphaSlider.value = 1.0
         
     }
     
     
     @IBAction func changedRedSlider() {
         redValue.text = String(redSlider.value)
+        checkColor.backgroundColor = UIColor (
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: CGFloat(alphaSlider.value)
+        )
+        
     }
     
     @IBAction func changedGreenSlider() {
         greenValue.text = String(greenSlider.value)
+        checkColor.backgroundColor = UIColor (
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: CGFloat(alphaSlider.value)
+        )
+        
     }
     
     @IBAction func changedBlueSlider() {
         blueValue.text = String(blueSlider.value)
+        checkColor.backgroundColor = UIColor (
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: CGFloat(alphaSlider.value)
+        )
+        
     }
-
+    
+    @IBAction func changeAlpha() {
+        alphaValue.text = String(alphaSlider.value)
+        checkColor.backgroundColor = UIColor (
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: CGFloat(alphaSlider.value)
+        )
+    }
 }
 
