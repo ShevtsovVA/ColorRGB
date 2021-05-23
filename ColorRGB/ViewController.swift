6715b9aa6ecab5822c2ac477b84a5b0a3f8d5ed4
ShevtsovVA <v.a.shevtsov@yandex.ru>
2021-05-23 18:15:33 +0300

    added functional

--- ColorRGB/ViewController.swift
+++ ColorRGB/ViewController.swift
@@ -8,12 +8,55 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var redValue: UILabel!
+    @IBOutlet weak var greenValue: UILabel!
+    @IBOutlet weak var blueValue: UILabel!
+    
+    @IBOutlet weak var checkColor: UIView!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+//        redSlider.minimumValue = 0
+//        redSlider.maximumValue = 255
+        redSlider.tintColor = .red
+        
+//        greenSlider.minimumValue = 0
+//        greenSlider.maximumValue = 255
+        greenSlider.tintColor = .green
+        
+//        blueSlider.minimumValue = 0
+//        blueSlider.maximumValue = 255
+        blueSlider.tintColor = .blue
+        
+        checkColor.backgroundColor = UIColor (
+        red: CGFloat(redSlider.value),
+        green: CGFloat(greenSlider.value),
+        blue: CGFloat(blueSlider.value),
+        alpha: 1.0
+        )
+        
     }
-
+    
+    
+    @IBAction func changedRedSlider() {
+        redValue.text = String(redSlider.value)
+    }
+    
+    @IBAction func changedGreenSlider() {
+        greenValue.text = String(greenSlider.value)
+    }
+    
+    @IBAction func changedBlueSlider() {
+        blueValue.text = String(blueSlider.value)
+    }
 
 }
 
